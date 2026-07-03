c9d3458da960f718a6d78a4e5c5ff1c2e21294ae
LetTTGACO <1225751694@qq.com>
2026-07-04 06:50:53 +0800

    fix: declare jetbrains jdk21 build toolchain

--- jetbrains/build.gradle.kts
+++ jetbrains/build.gradle.kts
@@ -23,8 +23,9 @@ dependencies {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_17
-    targetCompatibility = JavaVersion.VERSION_17
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
 }
 
 tasks.test {
